c0815a3626c58abcfe66b2397d1b9b1782b11350
Christopher Vundi <chris.vundi@fire-engine-red.com>
2018-03-22 21:21:32 +0300

    Add login links to registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,6 +68,14 @@
                                 </button>
                             </div>
                         </div>
+                        <hr>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <a href="{{ url('/login/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
+                                <a href="{{ url('/login/twitter') }}" class="btn btn-twitter" class="btn btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
+                                <a href="{{ url('/login/facebook') }}" class="btn btn-facebook" class="btn btn-facebook"><i class="fa fa-facebook"></i> Facebook</a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
